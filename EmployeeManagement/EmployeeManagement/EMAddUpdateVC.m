ed3e1c4a95c2d6e624958d11b49bfe805d5576f0
Abhishek Thapliyal <Abhishek@Abhisheks-MacBook-Air.local>
2017-03-10 03:24:42 +0530

    #1

--- EmployeeManagement/EmployeeManagement/EMAddUpdateVC.m
+++ EmployeeManagement/EmployeeManagement/EMAddUpdateVC.m
@@ -195,16 +195,20 @@
     [picker dismissViewControllerAnimated:YES completion:nil];
 }
 
+//============================================================================================================================================
+#pragma mark : UIPickerView DELEGATES
+//============================================================================================================================================
+
 - (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
 {
     return 1;
 }
-// The number of rows of data
+
 - (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
 {
     return self.pickerData.count;
 }
-// The data to return for the row and component (column) that's being passed in
+
 - (NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
 {
     return self.pickerData[row];
